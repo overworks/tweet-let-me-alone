d7d2cd682568df4473ad5707899067eddbed68a0
Minhyung Park <urlinee@gmail.com>
2018-08-02 16:21:58 +0900

    modified copyright in assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LetMeAlone")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018 Minhyung Park")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
